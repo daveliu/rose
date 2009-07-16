4aed105db7ca609dcd28086118970478b25478f1
wjp <wjp@wjp.(none)>
2009-07-16 15:54:04 +0800

    test file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,5 @@
 class HomeController < ApplicationController
   def index
-    f = File.new("#{RAILS_ROOT}/log/test", "w+")
-    f.close
     render :text => "home"
-  end
-
-  def test
-    f = File.new("#{RAILS_ROOT}/log/test", "r")
-    render :text => "aa#{f.to_s}"
   end
 end
