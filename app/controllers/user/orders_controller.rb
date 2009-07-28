10473a8f9df0ee8cc90658a678214f789c78043d
wjp <wjp@wjp.(none)>
2009-07-28 10:20:44 +0800

    order

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -73,7 +73,7 @@ class User::OrdersController < ApplicationController
 
   def create
     find_order_data
-    order = Order.new(params[:order].update(:order_status_id => 1, :total_price => @prices.sum))
+    order = Order.new(params[:order].update(:order_status_id => 1, :total_price => @prices.sum, :user_id => session[:user_id]))
 
     order.order_upgrade_level = OrderUpgradeLevel.new(:upgrade_level_id => @upgrade_level.id,
       :upgrade_level_price_id => @upgrade_level_price.id, :level_status_id => 1) if @upgrade_level && @upgrade_level_price
